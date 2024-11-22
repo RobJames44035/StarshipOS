73ec5bb351e11f696d9344792c4987b68103330a
rajames <RobJames44035@gmail.com>
2024-11-21 19:26:25 -0500

    Add VM run and GRUB config build scripts
    Introduce `run-vm.sh` for launching StarshipOS in QEMU and `build_grub_cfg.sh` to generate a GRUB configuration. These scripts streamline the development process by automating VM testing and boot configuration creation.

--- scripts/run-vm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-qemu-system-x86_64 -cdrom live_cd/build/StarshipOS.iso -boot d -m 512
-
--- starship/scripts/build_kernel.sh
+++ starship/scripts/build_kernel.sh
@@ -2,11 +2,12 @@
 # shellcheck disable=SC2164
 # shellcheck disable=SC2162
 if [ ! -d build ]; then
-  cd starship_kernel
+  cd ./starship_kernel
   make clean
-  make -J 4
+  make x86_64_defconfig
+  make
   cd ../
   mkdir -p build/boot
-  cp starship_kernel/arch/x86/boot/bzImage build/boot/starship
+  cp starship_kernel/arch/x86_64/boot/bzImage build/boot/starship
   make clean
 fi
