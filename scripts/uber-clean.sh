02474ea933cf12d2e6597b48fa4ca03c89e03752
rajames <RobJames44035@gmail.com>
2024-11-26 11:51:34 -0500

    Reorganize build clean-up and module order
    Reordered commands in `uber-clean.sh` to clean builds in a more logical sequence. Adjusted module order in `pom.xml` for better readability and consistency.

--- scripts/uber-clean.sh
+++ scripts/uber-clean.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-rm -rfv ./busybox/build
+clear
+
 rm -rfv ./starship/build
-rm -rfv ./initramfs/build
-rm -rfv ./grub/build
+rm -rfv ./busybox/build
 rm -rfv ./java/build
-rm -rfv ./live_cd/build
+
 rm -rfv ./system-bridge-cpp/build
 rm -rfv .system-bridge-java/build
 rm -rfv ./userland-java/build
+
+rm -rfv ./initramfs/build
+rm -rfv ./grub/build
+rm -rfv ./live_cd/build
+
 mvn clean
