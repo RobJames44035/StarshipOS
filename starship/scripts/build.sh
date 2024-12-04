1c0ab61f93027d36e3007272614f6020878e1635
RobJames440 <robjames440@gmail.com>
2024-12-04 14:42:02 -0500

    Remove commented-out execution block from pom.xml
    Deleted an obsolete execution block that was commented out in the pom.xml to improve readability and maintainability. This change helps in keeping the build configuration clean and reduces any potential confusion while navigating through the file.

--- starship/scripts/build.sh
+++ starship/scripts/build.sh
@@ -10,9 +10,8 @@ HOME="/home/rajames/PROJECTS/StarshipOS"
 STARSHIP_ROOT="${HOME}/starship"
 KERNEL_DIR="${STARSHIP_ROOT}/starship_kernel"
 BUILD_DIR="${STARSHIP_ROOT}/build"
-BOOT_DIR="${BUILD_DIR}/boot"
-#KERNEL_IMAGE_PATH="${KERNEL_DIR}/arch/x86_64/boot/bzImage"
-#TARGET_KERNEL_PATH="${BOOT_DIR}/starship"
+LIVE_CD_PATH="$HOME/build/init_ram_fs" # Really for the live cd
+export INSTALL_MOD_PATH=$LIVE_CD_PATH
 
 # Logging function
 function log() {
@@ -22,37 +21,22 @@ function log() {
 log "Starting kernel build script."
 
 if [ ! -d "$BUILD_DIR" ]; then
-  log "Building kernel as build directory does not exist..."
-
+  log "Building kernel because the build directory does not exist."
   cd "$KERNEL_DIR"
-  cp "$STARSHIP_ROOT/.config" "$KERNEL_DIR/.config"
-
-  log "Building the starship kernel in ${KERNEL_DIR}"
+  cp "$TARSHIP_ROOT/.config" "$KERNEL_DIR/.config"
+  make mrproper # Make it REALLY clean.
+#  make xconfig  # Let's call xconfig to tinker with the kernel, but mostly we just save and exit.
+  log "Building the starship kernel in $KERNEL_DIR"
   make -j$(nproc) bzImage
-  sudo make modules
+  sudo make modules # Build kernel modules.
+  log "mkdir -p ${LIVE_CD_PATH}"
+  mkdir -p "$LIVE_CD_PATH"
+  log "make modules_install"
+  make modules_install
 
-# Step 3: Create a temporary directory for the live CD structure
-  LIVECD_PATH="${HOME}/starship/build/init_ram_fs" # Really for the live cd
-  log "mkdir -p ${LIVECD_PATH}"
-
-  mkdir -p "$LIVECD_PATH"
-
-# Step 4: Install modules into the temporary directory
-    log "export INSTALL_MOD_PATH=${LIVECD_PATH}"
-    export INSTALL_MOD_PATH=$LIVECD_PATH
-    log "make modules_install"
-    make modules_install
-
-# Step 5: Create boot directory and copy bzImage
-  log "mkdir -p ${LIVECD_PATH}/boot"
-  mkdir -p "${LIVECD_PATH}/boot"
-
-  log "cp arch/x86/boot/bzImage ${LIVECD_PATH}/boot/starship"
-  cp "arch/x86/boot/bzImage" "${LIVECD_PATH}/boot/starship"
-
-  mkdir -p $HOME/live_cd/build/iso-image
-
-  cp -rv $HOME/starship/build/live_cd/* $HOME/live_cd/build/iso-image
+  mkdir -p "${LIVE_CD_PATH}/boot"
+  log "cp arch/x86/boot/bzImage $LIVE_CD_PATH/boot/starship"
+  cp "arch/x86/boot/bzImage" "$LIVE_CD_PATH/boot/starship"
 
 else
     log "Nothing to do!"
