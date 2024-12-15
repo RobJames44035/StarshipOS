47f9bed5323b8530ce816094f98f071fa027ab2a
RobJames440 <robjames440@gmail.com>
2024-12-15 10:04:12 -0500

    Add rootmount.txt file for StarshipOS QCOW2 image structure
    This file documents the directory structure and symlinks of the QCOW2 image root mount. It provides a detailed hierarchical view of binaries and associated links, aiding in understanding and managing the StarshipOS environment.

--- qcow2_image/scripts/build.sh
+++ qcow2_image/scripts/build.sh
@@ -1,145 +1,122 @@
 #!/bin/bash
-# shellcheck disable=SC2164
 
-set -e  # Exit immediately if a command exits with a non-zero status
-set -u  # Treat unset variables as an error
+# Exit on errors or unset variables
+set -e
+set -u
 
+# Directories and configuration
 HOME="/home/rajames/PROJECTS/StarshipOS"
-THE_COW="$HOME/qcow2_image"
-FINAL_BUILD_DIR="$THE_COW/build"
+QCOW2_DIR="$HOME/qcow2_image"
+BUILD_DIR="$QCOW2_DIR/build"
+HDD_NAME="starship-os.qcow2"
+HDD_SIZE="4G"
+BOOT_MOUNT="$BUILD_DIR/boot"
+ROOT_MOUNT="$BUILD_DIR/mnt"
 
-TARGET_DIR="$THE_COW/target"
-
-ROOT_MOUNT_POINT="$FINAL_BUILD_DIR/mnt"
-BOOT_MOUNT_POINT="$FINAL_BUILD_DIR/boot"
-
-HDD_VIRT_NAME="starship-os.qcow2"
-NDB0="/dev/nbd0"
-# shellcheck disable=SC2034
-BIOS_PART="/dev/nbd0p1" # BIOS Boot partition (1MB)
-BOOT_PART="/dev/nbd0p2" # boot partition
-ROOT_PART="/dev/nbd0p3" # root partition
+# Cleanup function
+cleanup() {
+    sudo umount "$BOOT_MOUNT" &>/dev/null || true
+    sudo umount "$ROOT_MOUNT" &>/dev/null || true
+    [[ -n "${LOOP_DEVICE:-}" ]] && sudo losetup -d "$LOOP_DEVICE" &>/dev/null || true
+    rm -rf "$BOOT_MOUNT" "$ROOT_MOUNT"
+}
+trap cleanup EXIT
 
 function log() {
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] $1"
-#    read -p "breakpoint"
 }
 
-if [ ! -d build ]; then
-  mkdir -p "$FINAL_BUILD_DIR"
-  qemu-img create -f qcow2 "$FINAL_BUILD_DIR/$HDD_VIRT_NAME" 4G
-
-# Connect the qcow2 image as a block device
-  log "Connect the qcow2 image as a block device."
-  sudo modprobe nbd max_part=16
-  sudo qemu-nbd --connect="$NDB0" "$FINAL_BUILD_DIR/$HDD_VIRT_NAME"
-
-  # Create the partition table
-  log "Create the partition table."
-  sudo parted -s "$NDB0" -- mklabel gpt
-
-  # Create BIOS Boot partition (1MB)
-  log "Create BIOS Boot partition (1MB)."
-  sudo parted -s "$NDB0" -- mkpart primary 1MiB 2MiB
-  sudo parted -s "$NDB0" -- set 1 bios_grub on
-
-  # Create /boot partition (1GB)
-  log "Create /boot partition (1GB)."
-  sudo parted -s "$NDB0" -- mkpart primary ext4 2MiB 1026MiB
-
-  # Create root (/) partition with remaining space
-  log "Create root (/) partition with remaining space."
-  sudo parted -s "$NDB0" -- mkpart primary ext4 1026MiB 100%
-
-  # Format the /boot partition
-  log "Format the /boot partition (ext4)."
-  sudo mkfs.ext4 "$BOOT_PART"
-
-  # Format the root (/) partition
-  log "Format the root (/) partition (ext4)."
-  sudo mkfs.ext4 "$ROOT_PART"
-
-  log "Create $ROOT_MOUNT_POINT mount point and mounting."
-  mkdir -p "$ROOT_MOUNT_POINT"
-  sudo mount "$ROOT_PART" "$ROOT_MOUNT_POINT"
-
-  log "Creating $BOOT_MOUNT_POINT mount point and mounting."
-  mkdir -p "$BOOT_MOUNT_POINT"
-  sudo mount "$BOOT_PART" "$BOOT_MOUNT_POINT"
-
-  #######################################
-  # Add content to boot partition /boot #
-  #######################################
-  echo "#######################################"
-  echo "# Add content to boot partition /boot #"
-  echo "#######################################"
-
-  log "Copying kernel."
-  sudo cp -p "./target/kernel/build/init_ram_fs/boot/starship" "build/boot/starship"
-
-  log "Copy kernel modules."
-  sudo cp -rp "./target/kernel/build/init_ram_fs/lib" "build/boot"
-
-  log "Copying initramfs."
-  sudo cp -p "target/ramdisks/build/initramfs.hd0.gz" "build/boot"
-
-  sudo mkdir -p "$BOOT_MOUNT_POINT/grub"
-  log "Copying grub.cfg"
-  sudo cp -p "./target/grub/hd0/init_ram_fs/boot/grub.cfg" "build/boot/grub/grub.cfg"
-
-  # Install GRUB bootloader
-  log "Installing GRUB bootloader."
-  sudo grub-install --target=i386-pc --boot-directory="$BOOT_MOUNT_POINT" "$NDB0"
-
-#  tree "build/boot"
-  ########################################
-  #    Add content to root partition /   #
-  ########################################
-  echo "########################################"
-  echo "#    Add content to root partition /   #"
-  echo "########################################"
-  sudo mkdir -p build/mnt/bin \
-         build/mnt/dev \
-         build/mnt/etc \
-         build/mnt/home \
-         build/mnt/lib \
-         build/mnt/lib64 \
-         build/mnt/mnt \
-         build/mnt/opt \
-         build/mnt/proc \
-         build/mnt/root \
-         build/mnt/sbin \
-         build/mnt/sys \
-         build/mnt/tmp \
-         build/mnt/usr/bin \
-         build/mnt/usr/lib \
-         build/mnt/usr/lib64 \
-         build/mnt/usr/sbin \
-         build/mnt/var/log \
-         build/mnt/var/tmp \
-         build/mnt/var/run
-
-  # BusyBox
-  log "Copying BusyBox."
-  sudo cp -rp "./target/busybox/build/init_ram_fs/bin" "build/mnt"
-  sudo cp -rp "./target/busybox/build/init_ram_fs/sbin" "build/mnt"
-
-  # Java JDK
-  log "Copying JDK23."
-  sudo mkdir -p "build/mnt/java/jdk"
-  sudo cp -rp "./target/java/build/jdk" "build/mnt/java/jdk"
-###########################################################
-# You may add other content below as needed.
-###########################################################
-
-#tree "build/mnt"
-# Cleanup at the end
-
-  sudo rm -rf "$BOOT_MOUNT_POINT/*"
-  sudo umount --lazy "$BOOT_MOUNT_POINT"
-  sudo rm -rf "$ROOT_MOUNT_POINT/*"
-  sudo umount --lazy "$ROOT_MOUNT_POINT"
-  sudo qemu-nbd --disconnect /dev/nbd0
-else
-    log "Nothing to do.."
+# Step 1: Create QCOW2 image
+mkdir -p "$BUILD_DIR"
+if [ ! -f "$BUILD_DIR/$HDD_NAME" ]; then
+    log "Creating QCOW2 disk image ($HDD_NAME) of size $HDD_SIZE."
+    qemu-img create -f qcow2 "$BUILD_DIR/$HDD_NAME" "$HDD_SIZE"
 fi
+
+# Convert QCOW2 to raw for block device compatibility
+RAW_FILE="$BUILD_DIR/starship-os.raw"
+qemu-img convert -f qcow2 -O raw "$BUILD_DIR/$HDD_NAME" "$RAW_FILE"
+
+# Attach raw image to a loopback device
+LOOP_DEVICE=$(sudo losetup --find --show --partscan "$RAW_FILE")
+
+# Step 2: Partition the disk (including BIOS Boot Partition)
+sudo parted -s "$LOOP_DEVICE" mklabel gpt
+sudo parted -s "$LOOP_DEVICE" mkpart primary 1MiB 2MiB     # BIOS Boot Partition
+sudo parted -s "$LOOP_DEVICE" set 1 bios_grub on          # Mark as BIOS Boot
+sudo parted -s "$LOOP_DEVICE" mkpart primary ext4 2MiB 1024MiB # Boot Partition
+sudo parted -s "$LOOP_DEVICE" mkpart primary ext4 1024MiB 100% # Root Partition
+sudo partprobe "$LOOP_DEVICE"
+
+# Step 3: Format the boot and root partitions
+BOOT_PART="${LOOP_DEVICE}p2"
+ROOT_PART="${LOOP_DEVICE}p3"
+sudo mkfs.ext4 "$BOOT_PART"
+sudo mkfs.ext4 "$ROOT_PART"
+
+# Step 4: Mount the partitions
+mkdir -p "$BOOT_MOUNT" "$ROOT_MOUNT"
+sudo mount "$BOOT_PART" "$BOOT_MOUNT"
+sudo mount "$ROOT_PART" "$ROOT_MOUNT"
+
+# Step 5: Setup bootloader files on the boot partition
+sudo cp -p "./target/kernel/build/init_ram_fs/boot/starship" "$BOOT_MOUNT/starship"
+sudo cp -rp "./target/kernel/build/init_ram_fs/lib" "$BOOT_MOUNT"
+sudo cp -p "./target/ramdisks/build/initramfs.hd0.gz" "$BOOT_MOUNT"
+sudo mkdir -p "$BOOT_MOUNT/grub"
+sudo cp -p "./target/grub/hd0/init_ram_fs/boot/grub.cfg" "$BOOT_MOUNT/grub/grub.cfg"
+
+# Install GRUB on the device
+log "Installing GRUB..."
+sudo grub-install --target=i386-pc --boot-directory="$BOOT_MOUNT" "$LOOP_DEVICE"
+
+# Step 6: Populate the root filesystem
+log "Populating root filesystem with necessary directories."
+sudo mkdir -p "$ROOT_MOUNT/bin"
+sudo mkdir -p "$ROOT_MOUNT/dev"
+sudo mkdir -p "$ROOT_MOUNT/etc"
+sudo mkdir -p "$ROOT_MOUNT/home"
+sudo mkdir -p "$ROOT_MOUNT/lib"
+sudo mkdir -p "$ROOT_MOUNT/lib64"
+sudo mkdir -p "$ROOT_MOUNT/mnt"
+sudo mkdir -p "$ROOT_MOUNT/opt"
+sudo mkdir -p "$ROOT_MOUNT/proc"
+sudo mkdir -p "$ROOT_MOUNT/root"
+sudo mkdir -p "$ROOT_MOUNT/sbin"
+sudo mkdir -p "$ROOT_MOUNT/sys"
+sudo mkdir -p "$ROOT_MOUNT/tmp"
+sudo mkdir -p "$ROOT_MOUNT/usr/bin"
+sudo mkdir -p "$ROOT_MOUNT/usr/lib"
+sudo mkdir -p "$ROOT_MOUNT/usr/lib64"
+sudo mkdir -p "$ROOT_MOUNT/usr/sbin"
+sudo mkdir -p "$ROOT_MOUNT/var/log"
+sudo mkdir -p "$ROOT_MOUNT/var/tmp"
+sudo mkdir -p "$ROOT_MOUNT/var/run"
+
+# Copy BusyBox binaries
+log "Copying BusyBox binaries to root filesystem."
+sudo cp -rp "./target/busybox/build/init_ram_fs/bin" "$ROOT_MOUNT"
+sudo cp -rp "./target/busybox/build/init_ram_fs/sbin" "$ROOT_MOUNT"
+
+log "Adding Java JDK to root filesystem."
+sudo mkdir -p "$ROOT_MOUNT/java/jdk"
+sudo cp -rp "target/java/build/jdk" "$ROOT_MOUNT/java/jdk"
+
+# Visual check of directory structure
+log "Tree of boot partition:"
+tree "$BOOT_MOUNT" > bootmount.txt
+
+log "Tree of root partition:"
+tree "$ROOT_MOUNT" > rootmount.txt
+
+# Step 7: Unmount and detach
+log "Unmounting partitions and detaching loop device."
+sudo umount "$BOOT_MOUNT"
+sudo umount "$ROOT_MOUNT"
+sudo losetup -d "$LOOP_DEVICE"
+
+# Recreate the QCOW2 file after modifications
+qemu-img convert -f raw -O qcow2 "$RAW_FILE" "$BUILD_DIR/$HDD_NAME"
+rm -f "$RAW_FILE"
+
+log "Disk setup with bootloader and root filesystem completed successfully."
