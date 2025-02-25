dbfe43dffbe93468e7dd26a80795f701ca6b73b6
RobJames44035 <RobJames44035@gmail.com>
2025-02-25 18:30:13 -0500

    Add JNI build and install process with Maven exec plugin
    Integrated Maven exec plugin to automate JNI header generation, native library compilation, and installation into the root filesystem. Updated `install.sh` to include functions for mounting the rootfs, copying files, and unmounting. These changes streamline the build process for the JNI-based `libstarshipclib.so` library.

--- starship-sdk/scripts/install.sh
+++ starship-sdk/scripts/install.sh
@@ -4,3 +4,25 @@
 #
 # Licensed under GPL2, GPL3 and Apache 2
 #
+
+function mount_rootfs() {
+  LOOPDEV=$(sudo losetup -fP --show ../../buildroot/buildroot/output/images/rootfs.ext4)
+  sudo mount -o loop ../../buildroot/buildroot/output/images/rootfs.ext4 /mnt/rootfs
+}
+
+function copy_files() {
+  sudo cp -v target/libstarshipclib.so /mnt/rootfs/java/lib
+}
+
+function unmount_rootfs() {
+  sudo sync
+  sudo umount /mnt/rootfs/
+}
+
+function main() {
+  echo "Installing libstarshipclib.so"
+  mount_rootfs
+  copy_files
+  unmount_rootfs
+}
+main
